c13afb39b32f8360346fe292acb93e4524e567d2
Simon Rydberg <simpor81@gmail.com>
2023-09-14 13:17:50 +0200

    Draws a texture

--- core/src/main/kotlin/se/simpor/screen/GameScreen.kt
+++ core/src/main/kotlin/se/simpor/screen/GameScreen.kt
@@ -1,12 +1,43 @@
 package se.simpor.screen
 
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.utils.Scaling
+import com.badlogic.gdx.utils.viewport.ExtendViewport
 import ktx.app.KtxScreen
 import ktx.log.logger
 
 class GameScreen : KtxScreen {
-
+    private val stage = Stage(ExtendViewport(16f, 9f))
+    private val texture = Texture("assets/graphics/characters.png")
     override fun show() {
         log.debug { "GameScreen get shown" }
+        stage.addActor(
+            Image(texture).apply {
+                setPosition(1f, 1f)
+                setSize(1f, 1f)
+                setScaling(Scaling.fill)
+            }
+        )
+    }
+
+    override fun resize(width: Int, height: Int) {
+        with(stage) {
+            stage.viewport.update(width, height, true)
+        }
+    }
+
+    override fun render(delta: Float) {
+        with(stage) {
+            act(delta)
+            draw()
+        }
+    }
+
+    override fun dispose() {
+        stage.dispose()
+        texture.dispose()
     }
 
     companion object {
