5c7980fe7c266a9c7eec0331766c92565f495750
Simon Rydberg <simpor81@gmail.com>
2023-09-15 15:11:22 +0200

    Fixed animation spawn issues

--- core/src/main/kotlin/se/simpor/component/SpawnComponent.kt
+++ core/src/main/kotlin/se/simpor/component/SpawnComponent.kt
@@ -1,8 +1,12 @@
 package se.simpor.component
 
 import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.github.quillraven.fleks.Component
 import com.github.quillraven.fleks.ComponentType
+import com.github.quillraven.fleks.Entity
+import com.github.quillraven.fleks.World
+import ktx.log.logger
 import ktx.math.vec2
 
 data class SpawnConfig(
@@ -11,10 +15,34 @@ data class SpawnConfig(
 
 data class SpawnComponent(
     var type: String = "",
+    var animationModel: AnimationModel = AnimationModel.UNDEFINED,
+    var relativeSize: Vector2 = vec2(),
     var location: Vector2 = vec2()
 ) : Component<SpawnComponent> {
+
     companion object : ComponentType<SpawnComponent>() {
+        private val log = logger<SpawnComponent>()
+
     }
+
     override fun type(): ComponentType<SpawnComponent> = SpawnComponent
 
+
+    override fun World.onAdd(entity: Entity) {
+        with(entity[SpawnComponent]) {
+            entity {
+                log.info { "Spawning an entity: $animationModel" }
+                it += ImageComponent().apply {
+                    image = Image().apply {
+                        setPosition(location.x, location.y)
+                        setSize(relativeSize.x, relativeSize.y)
+                        setScaling(com.badlogic.gdx.utils.Scaling.fill)
+                    }
+                }
+                it += AnimationComponent().apply {
+                    nextAnimation(animationModel, se.simpor.component.AnimationType.IDLE)
+                }
+            }
+        }
+    }
 }
--- core/src/main/kotlin/se/simpor/system/EntitySpawnSystem.kt
+++ core/src/main/kotlin/se/simpor/system/EntitySpawnSystem.kt
@@ -4,8 +4,6 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.Event
 import com.badlogic.gdx.scenes.scene2d.EventListener
-import com.badlogic.gdx.scenes.scene2d.ui.Image
-import com.badlogic.gdx.utils.Scaling
 import com.github.quillraven.fleks.Entity
 import com.github.quillraven.fleks.IteratingSystem
 import com.github.quillraven.fleks.World
@@ -17,10 +15,13 @@ import ktx.tiled.layer
 import ktx.tiled.type
 import ktx.tiled.x
 import ktx.tiled.y
+import se.simpor.MysticWoodGame
 import se.simpor.MysticWoodGame.Companion.UNIT_SCALE
-import se.simpor.component.*
+import se.simpor.component.AnimationModel
+import se.simpor.component.AnimationType
+import se.simpor.component.SpawnComponent
+import se.simpor.component.SpawnConfig
 import se.simpor.event.MapChangedEvent
-import se.simpor.screen.GameScreen
 
 class EntitySpawnSystem(
     private val atlas: TextureAtlas = inject()
@@ -29,11 +30,23 @@ class EntitySpawnSystem(
     enabled = true
 ), EventListener {
 
-    private val cachedConfigs = mutableMapOf<String, SpawnConfig>()
     private val cachedSize = mutableMapOf<AnimationModel, Vector2>()
+    private val cachedConfigs = mutableMapOf<String, SpawnConfig>()
+
 
     companion object {
-        private val log = logger<GameScreen>()
+        private val log = logger<EntitySpawnSystem>()
+    }
+
+    private fun size(model: AnimationModel) = cachedSize.getOrPut(model) {
+        val regions = atlas.findRegions("${model.atlasKey}/${AnimationType.IDLE.atlasKey}")
+        if (regions.isEmpty) gdxError("There are no regions for the idle animation of model $model")
+
+        val firstFrame = regions.first()
+        vec2(
+            firstFrame.originalWidth * MysticWoodGame.UNIT_SCALE,
+            firstFrame.originalHeight * MysticWoodGame.UNIT_SCALE
+        )
     }
 
     private fun spawnConfig(type: String) = cachedConfigs.getOrPut(type) {
@@ -45,46 +58,21 @@ class EntitySpawnSystem(
         }
     }
 
-    private fun size(model: AnimationModel) = cachedSize.getOrPut(model) {
-        val regions = atlas.findRegions("${model.atlasKey}/${AnimationType.IDLE.atlasKey}")
-        if (regions.isEmpty) gdxError("There are no regions for the idle animation of model $model")
-
-        val firstFrame = regions.first()
-        vec2(firstFrame.originalWidth * UNIT_SCALE, firstFrame.originalHeight * UNIT_SCALE)
-    }
-
     override fun handle(event: Event): Boolean {
         log.info { "Spawning entities defined in map" }
         when (event) {
             is MapChangedEvent -> {
-
                 val entityLayer = event.map.layer("entities")
                 entityLayer.objects.forEach { mapObject ->
                     val type = mapObject.name ?: gdxError("Mapobject ${mapObject.type} does not have a type")
                     log.info { "Spawning $type" }
-                    val spawnComponent = SpawnComponent().apply {
-                        this.type = type
-                        this.location.set(mapObject.x * UNIT_SCALE, mapObject.y * UNIT_SCALE)
-                    }
+                    val spawnComponent = SpawnComponent(
+                        type = type,
+                        animationModel = spawnConfig(type).model,
+                        relativeSize = size(spawnConfig(type).model),
+                        location = vec2(mapObject.x * UNIT_SCALE, mapObject.y * UNIT_SCALE)
+                    )
                     world.entity { it += spawnComponent }
-                    with(spawnComponent) {
-                        val config = spawnConfig(this.type)
-                        val relativeSize = size(config.model)
-                        world.entity {
-                            log.info {"Spawning an entity: ${config.model}"}
-                            it += ImageComponent().apply {
-                                image = Image().apply {
-                                    setPosition(location.x, location.y)
-                                    setSize(relativeSize.x, relativeSize.y)
-                                    setScaling(com.badlogic.gdx.utils.Scaling.fill)
-                                }
-                            }
-                            it += AnimationComponent().apply {
-                                nextAnimation(config.model, se.simpor.component.AnimationType.IDLE)
-                            }
-                        }
-                    }
-
                 }
                 return true
             }
